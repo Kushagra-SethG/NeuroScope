8ff13b9f11c43435e81b79f86a4fd67945db3f55
Kushagra Agrawal <kushagraagrawal178@gmail.com>
2025-08-23 16:26:18 +0530

    Changes

--- App.py
+++ App.py
@@ -37,7 +37,7 @@ with st.sidebar:
     """)
     st.markdown("---")
     st.markdown("**Model:** YOLOv8n (Ultralytics)")
-    st.markdown("**App by:** Kushagra Agrawal")
+    st.markdown("**App by:** Team No. 79")
     st.markdown("**Date:** {}".format(datetime.now().strftime("%B %d, %Y")))
     st.markdown("---")
     st.info("This app is for educational/demo purposes only. Not for clinical use.")
